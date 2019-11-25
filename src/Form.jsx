947e0af91d42ba6018ce945dc193e971256008af
Assyl-Salah <salaha@student.mini.pw.edu.pl>
2019-11-25 16:57:02 +0100

    Lab7Task4

--- src/Form.jsx
+++ src/Form.jsx
@@ -9,7 +9,10 @@ constructor(props)
         phone :null,
         email : null ,
         phonevalid : true,
-        emailvalid :true 
+        emailvalid :true ,
+        pname : null ,
+        name : null ,
+        isdisabled : true
 
     }
     this.agehandler=this.agehandler.bind(this);
@@ -17,6 +20,14 @@ constructor(props)
    this.validationphone=this.validationphone.bind(this);
    this.emailhandler=this.emailhandler.bind(this);
    this.validationemail=this.validationemail.bind(this);
+   this.namehandler=this.namehandler.bind(this);
+   this.parentnamehandler=this.parentnamehandler.bind(this);
+}
+namehandler=(e)=>{
+    this.setState({name:e.target.value});
+}
+parentnamehandler=(e)=>{
+    this.setState({pname:e.target.value});
 }
 phonehandler=(e)=>{
     this.setState({phone:e.target.value});
@@ -31,8 +42,8 @@ this.setState({age:e.target.value});
 
     validationphone=(e)=>{
         if(this.state.age<18){
-            const phnorpattern=/^[0-9]{9}$/;
-            phnorpattern.test(this.state.phone) ? this.setState({phonevalid:true}) : this.setState({phonevalid:false});
+            const phonepattern=/^[0-9]{9}$/;
+            phonepattern.test(e.target.value)  ? this.setState({phonevalid:true , isdisabled:false} ) : this.setState({phonevalid:false , isdisabled:true});
 
         }
     }
@@ -40,34 +51,37 @@ this.setState({age:e.target.value});
     validationemail=(e)=>{
         
             const emailpattern= /^([\w.%+-]+)@([\w-]+\.)+([\w]{2,}|com|org|net)*$/;
-            emailpattern.test(this.state.email)? this.setState({emailvalid:true}) : this.setState({emailvalid:false});
+            emailpattern.test(e.target.value) ? this.setState({emailvalid:true , isdisabled:false}) : this.setState({emailvalid:false, isdisabled:true});
         }
+    
     
     
 render()
     {
         return(
-            <form onSubmit={e => {e.preventDefault(); this.validationphone() ; this.validationemail()}}>
+
+            <form onSubmit={e => {e.preventDefault();}}>
+                <fieldset>
 <label >Age :</label><input type="number" name="age" onChange={e=>this.agehandler(e)}></input>
 <br/>
 {this.state.age <18 ?(
     <div>
-<label>Parent Name: </label><input  type="text" name="name"></input>
+<label>Parent Name: </label><input  onChange={e=>this.parentnamehandler(e)} type="text" name="name"></input>
 <br/>
-<label >Parent Phone Number: </label><input   onChange={e=>this.phonehandler(e)} name="phone"></input>
+<label >Parent Phone Number: </label><input  onChange={(e) => {this.phonehandler(e); this.validationphone(e);}} name="phone"></input>
 {!this.state.phonevalid ? <label>wrong phone number</label> : null}
 <br/>
-<button type="submit"  >Submit</button> 
 </div>)
 :(
     <div>
-<label> Name: </label><input  type="text" name="name"></input>
+<label> Name: </label><input  onChange={e=>this.namehandler(e)} type="text" name="name"></input>
 <br></br>
-<labe>Email: </labe><input name="email" onChange={e=>this.emailhandler(e)}></input>
+<label>Email: </label><input name="email" onChange={(e) => {this.emailhandler(e); this.validationemail(e);}}></input>
 {!this.state.emailvalid? <label>wrong email address</label> : null}
 <br></br>
-<button type="submit"  >Submit</button> 
-</div> )}         
+</div> )}     
+<button type="submit" disabled={this.state.isdisabled} >Submit</button>    
+</fieldset>
             </form>
         )
 
